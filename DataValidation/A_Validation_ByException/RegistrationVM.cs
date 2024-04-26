6d3cf2c9a0b950db61ac066ff4ad31262ba5a76d
vungvan123 <vungvan321@gmail.com>
2024-04-26 16:56:57 +0700

    first commit

---
+++ DataValidation/A_Validation_ByException/RegistrationVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Validation_ByException
+{
+    public class RegistrationVM : ObservableObject
+    {
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Username cannot be empty.");
+
+                OnPropertyChanged(ref _username, value);
+            }
+        }
+    }
+}
